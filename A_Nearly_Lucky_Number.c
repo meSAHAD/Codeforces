6b34d6b9f65e901f62d4b806ec7416729151dae8
meSAHAD <sahad.cse8.bu@gmail.com>
2023-04-01 17:20:05 +0600

    setup

---
+++ A_Nearly_Lucky_Number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main ()
+{
+    int  count=0;
+    char a[100];
+    scanf("%s",a);
+    for(int i=0; i<strlen(a); i++)
+    {
+        if (a[i]=='4' || a[i]=='7') count++;
+    }
+   
+    if (count ==4 || count ==7) printf("YES\n");
+    else printf("NO\n");
+    return 0;
+    
+}
